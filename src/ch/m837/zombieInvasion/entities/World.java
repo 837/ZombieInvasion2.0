41430bbb01b79b092c16377c80185d7badd5ff60
837 <ernst.matthias@hotmail.com>
2015-05-23 01:19:06 +0200

    added Entity removed IEntity added World

---
+++ src/ch/m837/zombieInvasion/entities/World.java
@@ -0,0 +1,15 @@
+package ch.m837.zombieInvasion.entities;
+
+import java.util.ArrayList;
+
+public class World {
+  ArrayList<Entity> entities = new ArrayList<>();
+
+  static public void sendEventToEntity() {
+
+  }
+
+  static public void sendEventToEntities() {
+
+  }
+}
