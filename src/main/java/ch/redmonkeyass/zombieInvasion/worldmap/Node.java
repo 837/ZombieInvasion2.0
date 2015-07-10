1ea4e9a5f182f63c249ee73b3b921e42aab33be2
837 <837@gmx.ch>
2015-07-10 15:58:40 +0200

    PathFinding work in progress

--- src/main/java/ch/redmonkeyass/zombieInvasion/worldmap/Node.java
+++ src/main/java/ch/redmonkeyass/zombieInvasion/worldmap/Node.java
@@ -4,12 +4,22 @@ import com.badlogic.gdx.physics.box2d.Body;
 
 import ch.redmonkeyass.zombieInvasion.worldmap.WorldMap.FieldType;
 
-public class Node {
+public class Node implements Comparable {
   private final Body body;
   private final FieldType type;
 
   private final float width, height;
   private final float x, y;
+
+
+  /** The path cost for this node */
+  private float cost;
+  /** The parent of this node, how we reached it in the search */
+  private Node parent;
+  /** The heuristic cost of this node */
+  private float heuristic;
+  /** The search depth of this node */
+  private int depth;
 
   public Node(float width, float height, float x, float y, Body body, FieldType type) {
     this.body = body;
@@ -42,5 +52,38 @@ public class Node {
 
   public float getY() {
     return y;
+  }
+
+  public boolean isSame(Node other) {
+    return (other.x == this.x && other.y == this.y);
+  }
+
+  /**
+   * Set the parent of this node
+   * 
+   * @param parent The parent node which lead us to this node
+   * @return The depth we have no reached in searching
+   */
+  public int setParent(Node parent) {
+    depth = parent.depth + 1;
+    this.parent = parent;
+
+    return depth;
+  }
+
+  @Override
+  public int compareTo(Object other) {
+    Node o = (Node) other;
+
+    float f = heuristic + cost;
+    float of = o.heuristic + o.cost;
+
+    if (f < of) {
+      return -1;
+    } else if (f > of) {
+      return 1;
+    } else {
+      return 0;
+    }
   }
 }
