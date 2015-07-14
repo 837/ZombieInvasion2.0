495aa10e2ddb0adf733a124279417688358ac5df
BGIIIHunterIII <p.del.conte@hotmail.ch>
2015-07-14 12:57:31 +0200

    Camera has better documentation (renamed some private methods) fixed Camera initialization added bgetPositionOnScreen and bOnCamera -- can be used to determine if something has to be rendered

--- src/main/java/ch/redmonkeyass/zombieInvasion/camera/Camera.java
+++ src/main/java/ch/redmonkeyass/zombieInvasion/camera/Camera.java
@@ -3,6 +3,7 @@ package ch.redmonkeyass.zombieInvasion.camera;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.state.StateBasedGame;
 
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 import ch.redmonkeyass.zombieInvasion.Config;
@@ -11,7 +12,12 @@ import ch.redmonkeyass.zombieInvasion.WorldHandler;
 public class Camera {
   private final int viewport_size_X;
   private final int viewport_size_Y;
-  private Vector2 position = new Vector2();
+
+  private Vector2 position;
+  /**
+   * update before use
+   */
+  Rectangle screenRect;
   private float offsetMaxX;
   private float offsetMaxY;
   private float offsetMinX;
@@ -24,6 +30,8 @@ public class Camera {
     offsetMinX = viewport_size_X / 2;
     offsetMinY = viewport_size_Y / 2;
     setMapData(mapWidth, mapHeight);
+    position = Vector2.Zero.add(viewport_size_X/2,viewport_size_Y/2);
+    screenRect = new Rectangle(getPosition().x,getPosition().y,viewport_size_X,viewport_size_Y);
   }
 
   public void UPDATE(GameContainer gc, StateBasedGame sbg) {
@@ -35,8 +43,11 @@ public class Camera {
               e.getAdditionalInfo(Vector2[].class).ifPresent(positions -> {
                 Vector2 oldPos = positions[0].scl(Config.MOUSE_DRAG_SMOOTHNESS);
                 Vector2 newPos = positions[1].scl(Config.MOUSE_DRAG_SMOOTHNESS);
-                Vector2 movement = newPos.sub(oldPos);
-                move(movement);
+                Vector2 direction = newPos.sub(oldPos);
+                direction.x = (float) Math.floor(direction.x);
+                direction.y = (float) Math.floor(direction.y);
+
+                move(direction);
               });
               break;
           }
@@ -44,19 +55,15 @@ public class Camera {
   }
 
   public void move(Vector2 direction) {
-    setPosition(position.add(direction));
+    setPositionAndKeepWithinMapBoundaries(position.add(direction));
   }
 
   private void setMapData(int mapWidth, int mapHeight) {
     offsetMaxX = mapWidth * Config.B2PIX - viewport_size_X / 2;
     offsetMaxY = mapHeight * Config.B2PIX - viewport_size_Y / 2;
-    /**
-     * muss noch entfernt werden, workaround
-     */
-    move(new Vector2(0, -1));
   }
 
-  private float checkOffset(float lowerBound, float upperBound, float number) {
+  private float keepWithinBoundaries(float lowerBound, float upperBound, float number) {
     if (number < lowerBound) {
       return lowerBound;
     }
@@ -66,22 +73,50 @@ public class Camera {
     return number;
   }
 
+  /**
+   *
+   * @return top left corner of the camera window
+   */
   public Vector2 getPosition() {
     return new Vector2(position.x - viewport_size_X / 2, position.y - viewport_size_Y / 2);
   }
 
-  private void setPosition(Vector2 point) {
-    position = new Vector2(checkOffset(offsetMinX, offsetMaxX, point.x),
-            checkOffset(offsetMinY, offsetMaxY, point.y));
-    System.out.println("Camera moved " + position.toString());
+  private void setPositionAndKeepWithinMapBoundaries(Vector2 point) {
+    position = new Vector2(keepWithinBoundaries(offsetMinX, offsetMaxX, point.x),
+            keepWithinBoundaries(offsetMinY, offsetMaxY, point.y));
+  }
+  /**
+   * pix to pix
+   * @param pos to be converted to screen coordinates
+   * @return a new vector towards position on screen in pix
+   */
+  public Vector2 getPositionOnScreen(Vector2 pos) {
+    return pos.cpy().sub(position);
   }
 
-  public Vector2 getPositionInWorld(Vector2 screenPos) {
-    return screenPos.add(getPosition());
+  /**
+   * box to pix
+   * @param pos to be converted to screen coordinates
+   * @return a new vector towards position on screen in pix
+   */
+  public Vector2 bgetPositionOnScreen(Vector2 pos){
+    return pos.cpy().scl(Config.B2PIX).sub(position);
   }
 
-  public Vector2 getPositionOnScreen(Vector2 screenPos) {
-    return screenPos.sub(getPosition());
+  /**
+   *
+   * @param pos in box coordinates
+   * @return true if pos is on the screen window
+   */
+  public boolean bOnCamera(Vector2 pos){
+    updateScreenRectangle();
+    return screenRect.contains(pos.cpy().scl(Config.B2PIX));
+  }
+
+
+
+  private void updateScreenRectangle(){
+    screenRect.setPosition(getPosition());
   }
 
   public int getViewport_size_X() {
